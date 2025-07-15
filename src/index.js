a3eae58f46f26b7f6c74e57981cb006a4f0c2dce
Monish <monishsaravanan0311@gmail.com>
2025-07-15 11:14:51 +0530

    update..

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import { CartProvider } from "./CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CartProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </CartProvider>
 );
 
